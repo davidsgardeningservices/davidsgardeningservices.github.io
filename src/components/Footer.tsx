958f5184b6d678f75cf29cfec8a761d5acb4549b
davidsgardeningservices <davidsgardeningservicesmedway@gmail.com>
2024-11-19 19:39:55 +0000

    footer color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import './Footer.css'
-import Container from '@mui/material/Container';
 import logo from '../images/Davids-Gardening-Logo.svg'
 import IconButton from '@mui/material/IconButton';
 import { Facebook, Instagram, X } from '@mui/icons-material';
@@ -7,25 +6,25 @@ import { Facebook, Instagram, X } from '@mui/icons-material';
 export default function Footer() {
     return (
         <div>
-            <div id='social-media'>
-                <IconButton>
-                    <Facebook color='success' style={{ color: '#649c2c' }} />
-                </IconButton>
-                <IconButton>
-                    <Instagram color='success' style={{ color: '#649c2c' }} />
-                </IconButton>
-                <IconButton>
-                    <X color='success' style={{ color: '#649c2c' }} />
-                </IconButton>
-            </div>
-            <Container id='footer'>
+            <div id='footer'>
+                <div id='social-media'>
+                    <IconButton>
+                        <Facebook color='success' style={{ color: '#649c2c' }} />
+                    </IconButton>
+                    <IconButton>
+                        <Instagram color='success' style={{ color: '#649c2c' }} />
+                    </IconButton>
+                    <IconButton>
+                        <X color='success' style={{ color: '#649c2c' }} />
+                    </IconButton>
+                </div>
                 <div id='logo'>
                     <img src={logo} alt="" />
                 </div>
-            </ Container>
-            <div id='copyright'>
-                &copy; Dr J. Guscott 2024
-            </div>
+                <div id='copyright'>
+                    &copy; Dr J. Guscott 2024
+                </div>
+            </ div>
         </div>
     )
 }
